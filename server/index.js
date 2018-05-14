d92e4b3f849ab6b8b4280b0275fe49bfb4d309a8
Jennifer Anderson <jennifer@macpractice.com>
2018-05-14 09:57:53 -0500

    Created a separate GET and POST endpoint for patients.

--- server/index.js
+++ server/index.js
@@ -4,14 +4,7 @@ var app = express();
 var bodyParser = require("body-parser");
 var mysql = require("mysql");
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-var port = process.env.por || 9009;
-var router = express.Router();
-
-app.use("/patients/1/patient", router);
-
+//database connection
 var connection = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -19,22 +12,45 @@ var connection = mysql.createConnection({
     database: "macpractice2"
 });
 
-router.get("/", function (req, res, next) {
-    connection.connect(function (err) {
-        if (err)
-	{
-		console.log(err);
-	}
-        connection.query("select person.first, person.last from patient, person where patient.person_id = person.person_id", function (err, result, fields) {
-            if (err) throw err;
-            res.send(JSON.stringify({
-                "status": 200,
-                "error": null,
-                "response": result
-            }));
-        });
+//setup json parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.por || 9009;
+var router = express.Router();
+
+app.route('/patients')
+    .get(function (req, res) {
+        connection.connect(function (err) {
+            if (err) {
+                console.log(err);
+            }
+            connection.query("select person.first, person.last from patient, person where patient.person_id = person.person_id", function (err, result, fields) {
+                if (err) throw err;
+                res.send(JSON.stringify({
+                    "status": 200,
+                    "error": null,
+                    "response": result
+                }));
+            });
+        })
+    })
+
+    .post(function(req, res) {
+        connection.connect(function (err) {
+            if (err) {
+                console.log(err);
+            }
+            connection.query("INSERT INTO patient (person_id) VALUES (18)", function (err, result, fields) {
+                if (err) throw err;
+                res.send(JSON.stringify({
+                    "status": 200,
+                    "error": null,
+                    "response": result
+                }));
+            });
+        })
     });
-});
 
 app.listen(port, function () {
     console.log("Express server running on port %d", port);

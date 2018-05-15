2835678200c6a2c956c1ad07e66f4c67d02f9a96
Jennifer Anderson <jennifer@macpractice.com>
2018-05-15 12:36:44 -0500

    Sent fake client data to the server using axios

--- form/src/Api.js
+++ form/src/Api.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import qs from 'qs';
 
 const apiUrl = 'http://localhost:9009/patients';
 
@@ -10,8 +11,22 @@ class PatientsApi {
   }
 
   createPatient = async() => {
+    const data = {
+      "first": "Fred",
+      "last": "test",
+      "salute": "Mr.",
+      "suffix": "B.",
+      "maiden": "Anderson",
+      "nickname": "Jen",
+      "birthday": "2018-05-15T15:32:27.635Z",
+      "pop_sex": "1",
+      "phone1": "402-430-3537",
+      "email": "cool@gmail.com"
+    };
+      
     try {
-      let res = await axios.post(apiUrl);
+      let res = await axios.post(
+          apiUrl,qs.stringify(data))
       return res.data || {};
     } catch(error) {
       alert('Error creating patient:' + error);

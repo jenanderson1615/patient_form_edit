565cc4860bd38f3681d3b2ffe90331cefaf51be9
Jennifer Anderson <jennifer@macpractice.com>
2018-05-23 09:10:42 -0500

    Got form working to show all fields and values if there is one

--- form/src/components/ViewPatientDemographics.js
+++ form/src/components/ViewPatientDemographics.js
@@ -6,13 +6,14 @@ import PatientFieldView from '../formmodel/PatientFieldView';
 import { observer } from 'mobx-react';
 
 class ViewPatientDemographics extends React.Component {
-// ------------------------------------------------------
     render() {
-        console.log(this.props.patient);
-        // return PatientDemographicFields.map((i, idx) => {
-        //     let value = this.props.store[i.name];
-            return <PatientFieldView patient={this.props.patient}/>
-        // });
+        return PatientDemographicFields.map((i, idx) => {
+            let fieldName = i.name;
+            let patient = this.props.patient.patient;
+            let fieldVal = patient[fieldName];
+            let label = i.label;
+            return <PatientFieldView label={label} value={fieldVal} />
+        });
     }
 }
 export default observer(ViewPatientDemographics);
--- form/src/formmodel/PatientFieldView.js
+++ form/src/formmodel/PatientFieldView.js
@@ -3,19 +3,12 @@ import Checkbox from 'material-ui/Checkbox';
 import {FormControl, FormControlLabel } from 'material-ui/Form';
 import { observer } from 'mobx-react';
 
-const PatientFieldView = ({ patient }) => {
-    console.log(patient.patient);
+const PatientFieldView = ({ label, value }) => {
     return (
         <div>
             <div>
-                <b>First Name </b>{patient.patient.first}
+                <b>{label} </b>{value}
             </div>
-            {/* <div>
-                <b>Last Name </b>{patient.last}
-            </div>
-            <div>
-                <b>Maiden Name </b>{patient.maiden}
-            </div> */}
         </div>
     )
 }
